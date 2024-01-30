de88e2605fe824ca78b25192c45e15be2e25a0a3
Mel <angel.melendez.c@gmail.com>
2024-01-30 15:25:18 -0600

    create basic erc20 token contract

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -5,6 +5,47 @@ import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 const Home: NextPage = () => {
   return (
     <>
+      <div className="w-full min-h-screen flex justify-center">
+        <div className="h-1/2">
+          <h1>Balance</h1>
+        </div>
+        <div className="overflow-x-auto w-2/3 py-8">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+              {/* row 2 */}
+              <tr className="hover">
+                <th>2</th>
+                <td>Hart Hagerty</td>
+                <td>Desktop Support Technician</td>
+                <td>Purple</td>
+              </tr>
+              {/* row 3 */}
+              <tr>
+                <th>3</th>
+                <td>Brice Swyre</td>
+                <td>Tax Accountant</td>
+                <td>Red</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center mb-8">
