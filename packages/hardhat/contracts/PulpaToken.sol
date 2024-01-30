97224c254552e8e371d999b48cfdd9c4b9ed9a0b
Mel <angel.melendez.c@gmail.com>
2024-01-30 15:25:18 -0600

    create basic erc20 token contract

---
+++ packages/hardhat/contracts/PulpaToken.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract PulpaToken is ERC20, ERC20Burnable, AccessControl, ERC20Permit {
+	bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+	constructor(
+		string memory tokenName_,
+		string memory tokenSymbol_,
+		address defaultAdmin_,
+		address minter_
+	) ERC20(tokenName_, tokenSymbol_) ERC20Permit(tokenName_) {
+		_grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin_);
+		_grantRole(MINTER_ROLE, minter_);
+	}
+
+	function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+		_mint(to, amount);
+	}
+}
--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,6 +1,5 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { Contract } from "ethers";
 
 /**
  * Deploys a contract named "YourContract" using the deployer account and
@@ -22,23 +21,21 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  const admin_address = process.env.ADMIN_ADDRESS;
+
+  await deploy("PulpaToken", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: ["Pulpa Frutero Club", "PULPA", admin_address, admin_address],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
   });
-
-  // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
 };
 
 export default deployYourContract;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployYourContract.tags = ["PulpaToken"];
