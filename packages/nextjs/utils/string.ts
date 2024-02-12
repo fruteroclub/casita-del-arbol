6a6af10b7a860e967614dfce175de21fab6f3dbe
Mel <angel.melendez.c@gmail.com>
2024-02-12 13:36:02 -0600

    update string function

--- packages/nextjs/utils/string.ts
+++ packages/nextjs/utils/string.ts
@@ -1,4 +1,4 @@
-export const truncateAddress = (address: string | undefined, start: number, end: number) => {
+export const truncateAddress = (address: string | undefined, start = 6, end = 4) => {
   if (!address) return "Could not format address...";
   return `${address.slice(0, start)}...${address.slice(0 - end)}`;
 };
